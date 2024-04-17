94722f5501cbc23228dd3a273268516e584c1dbb
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-04-17 11:58:53 -0500

    Activating SW. Conditioning caching with "Remember". If "inCache" is true, Remember will take care of caching; otherwise, the service worker handles caching.

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -24,19 +24,19 @@ const assets = wbManifest.filter((asset: { url: string, revision: string }) => {
   return asset.url.includes('src_modules');
 });
 
-// precacheAndRoute(wbManifest)
+precacheAndRoute(assets)
 
 const requestPOST = new Map<string, Request>()
 const sentPOST = new Map<string, any>()
 const QUEUE_NAME = 'requests'
 const CACHE_NAME = 'stale-while-revalidate'
 
-// const plugins = [
-//   new ExpirationPlugin({
-//     maxEntries: 50,
-//     maxAgeSeconds: 60 * 60 // 1 Hour
-//   }),
-// ]
+const plugins = [
+  new ExpirationPlugin({
+    maxEntries: 50,
+    maxAgeSeconds: 60 * 60 // 1 Hour
+  }),
+]
 
 const match = request => {
   const url = new URL(request.request.url)
@@ -48,18 +48,18 @@ const match = request => {
   return isApi && isRefresh && !cache
 }
 
-// registerRoute(match, new NetworkFirst({
-//   cacheName: CACHE_NAME,
-//   plugins
-// }))
+registerRoute(match, new NetworkFirst({
+  cacheName: CACHE_NAME,
+  plugins
+}))
 
-// registerRoute(
-//   request => !match(request), 
-//   new CacheFirst({
-//     cacheName: CACHE_NAME,
-//     plugins
-//   })
-// )
+registerRoute(
+  request => !match(request), 
+  new CacheFirst({
+    cacheName: CACHE_NAME,
+    plugins
+  })
+)
 
 const postMessage = (message: string) => {
   self.clients.matchAll()
@@ -119,55 +119,55 @@ const groupRequestsById = (entries) => {
   }, {})
 }
 
-// const queueStore = new QueueStore(QUEUE_NAME)
+const queueStore = new QueueStore(QUEUE_NAME)
 
-// const squashRequests = async () => {
-//   const entries = await queueStore.getAll()
+const squashRequests = async () => {
+  const entries = await queueStore.getAll()
 
-//   const groupedUpdateRequests = groupRequestsById(entries)
+  const groupedUpdateRequests = groupRequestsById(entries)
 
-//   try {
-//     await Promise.all(Object.keys(groupedUpdateRequests).map(async key => {
-//       const group = groupedUpdateRequests[key]
+  try {
+    await Promise.all(Object.keys(groupedUpdateRequests).map(async key => {
+      const group = groupedUpdateRequests[key]
   
-//       if (group.length > 1) {
-//         let body = null
+      if (group.length > 1) {
+        let body = null
   
-//         await Promise.all(group.map(async (item: any  ) => {
-//           const bodyArrayBuffer = item.requestData.body
+        await Promise.all(group.map(async (item: any  ) => {
+          const bodyArrayBuffer = item.requestData.body
 
-//           const { 
-//             attributes: attributesPUT 
-//           } = decode(bodyArrayBuffer)
+          const { 
+            attributes: attributesPUT 
+          } = decode(bodyArrayBuffer)
   
-//           await queueStore.deleteEntry(item.id)
+          await queueStore.deleteEntry(item.id)
   
-//           body = {
-//             attributes: {
-//               ...body?.attributes,
-//               ...attributesPUT
-//             }
-//           }
-//         }))
+          body = {
+            attributes: {
+              ...body?.attributes,
+              ...attributesPUT
+            }
+          }
+        }))
 
-//         const requestData = group[0].requestData
+        const requestData = group[0].requestData
   
-//         const mergeRequest = new Request(requestData?.url, {
-//           ...requestData,
-//           body: JSON.stringify(body),
-//         })
+        const mergeRequest = new Request(requestData?.url, {
+          ...requestData,
+          body: JSON.stringify(body),
+        })
   
-//         await queue.pushRequest({
-//           request: mergeRequest
-//         })
+        await queue.pushRequest({
+          request: mergeRequest
+        })
         
-//       }
-//     }))
-//   } catch (error) {
-//     console.log('error', error)
-//   }
+      }
+    }))
+  } catch (error) {
+    console.log('error', error)
+  }
 
-// }
+}
 
 const createNewRequest = (url, request, attr) => {
   console.log('request ->', request)
@@ -189,50 +189,50 @@ const createNewRequest = (url, request, attr) => {
   })
 }
 
-// const mergePostAndPutRequests = async (entry, entries) => {
-//   try {
-//     let body = null
-//     const { 
-//       attributes: { offline_id }, 
-//       attributes: attributesPOST 
-//     } = await transformReadableStreamToObject(entry.body)
+const mergePostAndPutRequests = async (entry, entries) => {
+  try {
+    let body = null
+    const { 
+      attributes: { offline_id }, 
+      attributes: attributesPOST 
+    } = await transformReadableStreamToObject(entry.body)
 
-//     await Promise.all(entries.map(async (request: any) => {
-//       if (request.requestData.method !== 'PUT') return
+    await Promise.all(entries.map(async (request: any) => {
+      if (request.requestData.method !== 'PUT') return
 
-//       const id = getIdFromUrl(request.requestData.url)
+      const id = getIdFromUrl(request.requestData.url)
 
-//       if (String(id) === String(offline_id)) {
-//         const bodyArrayBuffer = request.requestData.body
+      if (String(id) === String(offline_id)) {
+        const bodyArrayBuffer = request.requestData.body
 
-//         const { 
-//           attributes: attributesPUT 
-//         } = await decode(bodyArrayBuffer)
+        const { 
+          attributes: attributesPUT 
+        } = await decode(bodyArrayBuffer)
 
-//         delete attributesPUT.id
+        delete attributesPUT.id
 
-//         await queueStore.deleteEntry(request.id)
+        await queueStore.deleteEntry(request.id)
 
-//         body = JSON.stringify({
-//           attributes: {
-//             ...attributesPOST,
-//             ...attributesPUT,
-//           }
-//         })
-//       }
-//     }))
+        body = JSON.stringify({
+          attributes: {
+            ...attributesPOST,
+            ...attributesPUT,
+          }
+        })
+      }
+    }))
 
-//     if (body) {
-//       const mergeRequest = createNewRequest(entry.url, entry, { body })
+    if (body) {
+      const mergeRequest = createNewRequest(entry.url, entry, { body })
 
-//       requestPOST.delete(offline_id)
+      requestPOST.delete(offline_id)
 
-//       return mergeRequest
-//     }
-//   } catch (error) {
-//     console.log('Error merging requests', error)
-//   }
-// }
+      return mergeRequest
+    }
+  } catch (error) {
+    console.log('Error merging requests', error)
+  }
+}
 
 const replaceRequestUrlWithStoredUrl = async (clonedRequest, requestId) => {
   console.log('requestId de metadata', requestId)
@@ -251,126 +251,126 @@ const replaceRequestUrlWithStoredUrl = async (clonedRequest, requestId) => {
   } 
 }
 
-// const queue = new Queue(QUEUE_NAME, {
-//   onSync: async ({ queue }) => {
-//     let entry
-//     const retryCounters = new Map<string, number>()
+const queue = new Queue(QUEUE_NAME, {
+  onSync: async ({ queue }) => {
+    let entry
+    const retryCounters = new Map<string, number>()
     
-//     // Group PUT requests to then send a single PUT request.
-//     await squashRequests()
+    // Group PUT requests to then send a single PUT request.
+    await squashRequests()
     
-//     const entries = await queueStore.getAll()
+    const entries = await queueStore.getAll()
 
-//     while (entry = await queue.shiftRequest()) {
-//       try {
-//         if (entry.request.method === 'POST') {
-//           // If from the same POST request there are PUT requests, 
-//           // then it takes the corresponding PUT requests and merges 
-//           // them. Then it sends a single POST request.
-//           const mergeRequest = await mergePostAndPutRequests(entry.request.clone(), entries)
-//           if (mergeRequest) {
-//             entry.request = mergeRequest
-//           }
-//         }
+    while (entry = await queue.shiftRequest()) {
+      try {
+        if (entry.request.method === 'POST') {
+          // If from the same POST request there are PUT requests, 
+          // then it takes the corresponding PUT requests and merges 
+          // them. Then it sends a single POST request.
+          const mergeRequest = await mergePostAndPutRequests(entry.request.clone(), entries)
+          if (mergeRequest) {
+            entry.request = mergeRequest
+          }
+        }
 
-//         if (entry.request.method === 'DELETE') {
-//           // If a POST request is successful and then that same record 
-//           // is deleted during the disconnected state, then the fake ID 
-//           // contained in the URL is replaced by the real ID generated 
-//           // by the database as a response to the successful request.
-//           console.log('original method', entry.request.method)
-//           const newRequest = await replaceRequestUrlWithStoredUrl(entry.request.clone(), entry.metadata?.requestId)
-//           if (newRequest) {
-//             console.log('New method', newRequest.method)
-//             entry.request = newRequest
-//           }
-//         }
+        if (entry.request.method === 'DELETE') {
+          // If a POST request is successful and then that same record 
+          // is deleted during the disconnected state, then the fake ID 
+          // contained in the URL is replaced by the real ID generated 
+          // by the database as a response to the successful request.
+          console.log('original method', entry.request.method)
+          const newRequest = await replaceRequestUrlWithStoredUrl(entry.request.clone(), entry.metadata?.requestId)
+          if (newRequest) {
+            console.log('New method', newRequest.method)
+            entry.request = newRequest
+          }
+        }
 
-//         if (entry.metadata?.squash) return
-//         const response = await fetch(entry.request)
-//         console.log('response', response)
+        if (entry.metadata?.squash) return
+        const response = await fetch(entry.request)
+        console.log('response', response)
 
-//         if (entry.request.method === 'POST') {
-//           const { data } = await response.json()
-//           // Save the ID generated by the server 
-//           // to later use it in the URL of the DELETE 
-//           // request corresponding to the created record.
-//           sentPOST.set(data.offlineId, data.id)
-//         }
+        if (entry.request.method === 'POST') {
+          const { data } = await response.json()
+          // Save the ID generated by the server 
+          // to later use it in the URL of the DELETE 
+          // request corresponding to the created record.
+          sentPOST.set(data.offlineId, data.id)
+        }
 
-//         postMessage('successful')
+        postMessage('successful')
 
-//         retryCounters.delete(entry.request.url)
-//       } catch (error) {
-//         // If a queued request fails, it will try two more times, 
-//         // and if unsuccessful, it will remove it from the queue.
-//         const retryCounter = retryCounters.get(entry.request.url) || 0
+        retryCounters.delete(entry.request.url)
+      } catch (error) {
+        // If a queued request fails, it will try two more times, 
+        // and if unsuccessful, it will remove it from the queue.
+        const retryCounter = retryCounters.get(entry.request.url) || 0
 
-//         if (retryCounter < 3) {
-//           retryCounters.set(entry.request.url, retryCounter + 1);
-//           await queue.unshiftRequest(entry);
-//         } else {
-//           retryCounters.delete(entry.request.url);
-//         }
-//       }
-//     }
+        if (retryCounter < 3) {
+          retryCounters.set(entry.request.url, retryCounter + 1);
+          await queue.unshiftRequest(entry);
+        } else {
+          retryCounters.delete(entry.request.url);
+        }
+      }
+    }
 
-//     postMessage('sync-data')
-//   },
-// })
+    postMessage('sync-data')
+  },
+})
 
-// self.addEventListener('fetch', (event) => {
-//   const supportedMethods = [
-//     'POST',
-//     'PUT',
-//     'DELETE',
-//   ]
+self.addEventListener('fetch', (event) => {
+  const supportedMethods = [
+    'POST',
+    'PUT',
+    'DELETE',
+  ]
 
-//   if (!supportedMethods.includes(event.request.method)) {
-//     return
-//   }
+  if (!supportedMethods.includes(event.request.method)) {
+    return
+  }
 
-//   const bgSyncLogic = async () => {
-//     try {
-//       const response = await fetch(event.request.clone())
-//       return response
-//     } catch (error) {
-//       const path = new URL(event.request.url).pathname
-//       if(path.startsWith('/api/') && !navigator.onLine) {
+  const bgSyncLogic = async () => {
+    try {
+      const response = await fetch(event.request.clone())
+      return response
+    } catch (error) {
+      const path = new URL(event.request.url).pathname
+      if(path.startsWith('/api/') && !navigator.onLine) {
 
-//         let requestId = null
+        let requestId = null
 
-//         if (event.request.method === 'POST') {
-//           const { 
-//             attributes: { offline_id } 
-//           } = await transformReadableStreamToObject(event.request.clone().body)
-//           requestPOST.set(String(offline_id), event.request.clone())
-//         }
+        if (event.request.method === 'POST') {
+          const { 
+            attributes: { offline_id } 
+          } = await transformReadableStreamToObject(event.request.clone().body)
+          requestPOST.set(String(offline_id), event.request.clone())
+        }
 
-//         if (event.request.method === 'DELETE') {
-//           console.log('DELETE url', event.request.url)
-//           const id = getIdFromUrl(event.request.url)
-//           console.log('DELETE id', id)
+        if (event.request.method === 'DELETE') {
+          console.log('DELETE url', event.request.url)
+          const id = getIdFromUrl(event.request.url)
+          console.log('DELETE id', id)
 
-//           if (requestPOST.has(String(id))) {
-//             console.log('DELETE requestId', id)
-//             requestId = id
-//           }
-//         }
+          if (requestPOST.has(String(id))) {
+            console.log('DELETE requestId', id)
+            requestId = id
+          }
+        }
 
-//         await queue.pushRequest({
-//           request: event.request,
-//           metadata: {
-//             requestId
-//           }
-//         })
+        await queue.pushRequest({
+          request: event.request,
+          metadata: {
+            requestId
+          }
+        })
 
-//         postMessage('queue-request')
-//       }
+        postMessage('queue-request')
+      }
       
-//       return error
-//     }
-//   }
+      return error
+    }
+  }
 
-//   event.respondWith(bgSyncLogic())
-// })
+  event.respondWith(bgSyncLogic())
+})
